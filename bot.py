e007eaeed74bc7bd8efff099926ba9d68e790556
oeliasca <oeliasca@gmail.com>
2025-05-12 12:09:06 +0200

    first try poll

--- bot.py
+++ bot.py
@@ -34,12 +34,12 @@ logger = logging.getLogger(__name__)
 TOTAL_VOTER_COUNT = 3
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Inform user about what this bot can do"""
-    await update.message.reply_text(
-        "Please select /poll to get a Poll, /quiz to get a Quiz or /preview"
-        " to generate a preview for your poll"
-    )
+# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Inform user about what this bot can do"""
+#     await update.message.reply_text(
+#         "Please select /poll to get a Poll, /quiz to get a Quiz or /preview"
+#         " to generate a preview for your poll"
+#     )
 
 
 async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -80,53 +80,53 @@ async def receive_poll_answer(update: Update, context: ContextTypes.DEFAULT_TYPE
             answer_string += questions[question_id] + " and "
         else:
             answer_string += questions[question_id]
-    await context.bot.send_message(
-        answered_poll["chat_id"],
-        f"{update.effective_user.mention_html()} feels {answer_string}!",
-        parse_mode=ParseMode.HTML,
-    )
+    # await context.bot.send_message(
+    #     answered_poll["chat_id"],
+    #     f"{update.effective_user.mention_html()} feels {answer_string}!",
+    #     parse_mode=ParseMode.HTML,
+    # )
     answered_poll["answers"] += 1
     # Close poll after three participants voted
     if answered_poll["answers"] == TOTAL_VOTER_COUNT:
         await context.bot.stop_poll(answered_poll["chat_id"], answered_poll["message_id"])
 
 
-async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a predefined poll"""
-    questions = ["1", "2", "4", "20"]
-    message = await update.effective_message.reply_poll(
-        "How many eggs do you need for a cake?", questions, type=Poll.QUIZ, correct_option_id=2
-    )
-    # Save some info about the poll the bot_data for later use in receive_quiz_answer
-    payload = {
-        message.poll.id: {"chat_id": update.effective_chat.id, "message_id": message.message_id}
-    }
-    context.bot_data.update(payload)
-
-
-async def receive_quiz_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Close quiz after three participants took it"""
-    # the bot can receive closed poll updates we don't care about
-    if update.poll.is_closed:
-        return
-    if update.poll.total_voter_count == TOTAL_VOTER_COUNT:
-        try:
-            quiz_data = context.bot_data[update.poll.id]
-        # this means this poll answer update is from an old poll, we can't stop it then
-        except KeyError:
-            return
-        await context.bot.stop_poll(quiz_data["chat_id"], quiz_data["message_id"])
-
-
-async def preview(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Ask user to create a poll and display a preview of it"""
-    # using this without a type lets the user chooses what he wants (quiz or poll)
-    button = [[KeyboardButton("Press me!", request_poll=KeyboardButtonPollType())]]
-    message = "Press the button to let the bot generate a preview for your poll"
-    # using one_time_keyboard to hide the keyboard
-    await update.effective_message.reply_text(
-        message, reply_markup=ReplyKeyboardMarkup(button, one_time_keyboard=True)
-    )
+# async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Send a predefined poll"""
+#     questions = ["1", "2", "4", "20"]
+#     message = await update.effective_message.reply_poll(
+#         "How many eggs do you need for a cake?", questions, type=Poll.QUIZ, correct_option_id=2
+#     )
+#     # Save some info about the poll the bot_data for later use in receive_quiz_answer
+#     payload = {
+#         message.poll.id: {"chat_id": update.effective_chat.id, "message_id": message.message_id}
+#     }
+#     context.bot_data.update(payload)
+#
+#
+# async def receive_quiz_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Close quiz after three participants took it"""
+#     # the bot can receive closed poll updates we don't care about
+#     if update.poll.is_closed:
+#         return
+#     if update.poll.total_voter_count == TOTAL_VOTER_COUNT:
+#         try:
+#             quiz_data = context.bot_data[update.poll.id]
+#         # this means this poll answer update is from an old poll, we can't stop it then
+#         except KeyError:
+#             return
+#         await context.bot.stop_poll(quiz_data["chat_id"], quiz_data["message_id"])
+#
+#
+# async def preview(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     """Ask user to create a poll and display a preview of it"""
+#     # using this without a type lets the user chooses what he wants (quiz or poll)
+#     button = [[KeyboardButton("Press me!", request_poll=KeyboardButtonPollType())]]
+#     message = "Press the button to let the bot generate a preview for your poll"
+#     # using one_time_keyboard to hide the keyboard
+#     await update.effective_message.reply_text(
+#         message, reply_markup=ReplyKeyboardMarkup(button, one_time_keyboard=True)
+#     )
 
 
 async def receive_poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -153,14 +153,15 @@ def main() -> None:
     # Create the Application and pass it your bot's token.
     api_key = os.getenv('API_KEY')
     application = Application.builder().token(api_key).build()
-    application.add_handler(CommandHandler("start", start))
+
+    #application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("poll", poll))
-    application.add_handler(CommandHandler("quiz", quiz))
-    application.add_handler(CommandHandler("preview", preview))
+    #application.add_handler(CommandHandler("quiz", quiz))
+    #application.add_handler(CommandHandler("preview", preview))
     application.add_handler(CommandHandler("help", help_handler))
     application.add_handler(MessageHandler(filters.POLL, receive_poll))
     application.add_handler(PollAnswerHandler(receive_poll_answer))
-    application.add_handler(PollHandler(receive_quiz_answer))
+    #application.add_handler(PollHandler(receive_quiz_answer))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
